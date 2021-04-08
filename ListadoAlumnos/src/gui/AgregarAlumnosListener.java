52fc36171c65909e27c831548623b9bb190fd44b
Felipesorsilv <61020704+Felipesorsilv@users.noreply.github.com>
2021-04-08 15:14:05 -0500

    Add files via upload

---
+++ ListadoAlumnos/src/gui/AgregarAlumnosListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+import model.Alumno;
+import model.Carrera;
+/**
+ *
+ * @author felipe
+ */
+public interface AgregarAlumnosListener {
+    public void aceptarClick(Alumno alumno);
+}
